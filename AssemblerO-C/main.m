b881eb0c95a83825d33205e589bc98bff1ce3e4e
Kapil Garg <Kapil_Garg@Kapils-MacBook-Pro.local>
2012-12-03 13:09:34 -0600

    Initial Commit

---
+++ AssemblerO-C/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AssemblerO-C
+//
+//  Created by Kapil Garg on 12/3/12.
+//  Copyright (c) 2012 KapilAndrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
